3dc47384b3f97f5a484e6516f537cdef69fd4cfe
vikasc830 <83336342+vikasc830@users.noreply.github.com>
2025-07-15 21:28:09 +0530

    Fix Reports component export error

---
+++ src/pages/Reports.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Reports = () => {
+  return (
+    <div>
+      <h1>Reports</h1>
+      <p>Reports page content will go here.</p>
+    </div>
+  );
+};
+
+export default Reports;
